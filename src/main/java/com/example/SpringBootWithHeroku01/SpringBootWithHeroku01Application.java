d51bfc71e15bfa54e9426a8e3211e9d14f6c296e
HajPhan <hajphan97@gmail.com>
2019-08-17 21:41:17 +0700

    First commit :smile:

---
+++ src/main/java/com/example/SpringBootWithHeroku01/SpringBootWithHeroku01Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithHeroku01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithHeroku01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithHeroku01Application.class, args);
+	}
+
+}
